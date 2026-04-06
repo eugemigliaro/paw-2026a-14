af9f324a576c7a904a360d40220c587bfc6caac6
acragno <acragno@itba.edu.ar>
2026-04-06 16:59:49 -0300

    fix: add validation pattern for search query

--- webapp/src/main/java/ar/edu/itba/paw/webapp/form/FeedSearchForm.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/form/FeedSearchForm.java
@@ -1,10 +1,14 @@
 package ar.edu.itba.paw.webapp.form;
 
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class FeedSearchForm {
 
     @Size(max = 150, message = "Search cannot exceed 150 characters")
+    @Pattern(
+            regexp = "^[\\p{L}\\p{N} ]*$",
+            message = "Search must contain only letters, numbers, and spaces")
     private String q = "";
 
     public String getQ() {
