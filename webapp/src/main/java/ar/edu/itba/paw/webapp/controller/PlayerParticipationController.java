5c0348d54d26a7772fd2c8c5974ff84d826ba817
acragno <acragno@itba.edu.ar>
2026-06-03 18:18:21 -0300

    refactor: handle new exception in PlayerParticipationController

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PlayerParticipationController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/PlayerParticipationController.java
@@ -2,12 +2,31 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.services.MatchParticipationService;
-import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationAlreadyJoinedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationAlreadyPendingException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationClosedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationFullException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationInvalidUserException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationIsHostException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationNoInvitationException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationNoPendingRequestException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationNotFoundException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationNotInviteOnlyException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationNotRecurringException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationSeriesAlreadyJoinedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationSeriesAlreadyPendingException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationSeriesClosedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationSeriesFullException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationSeriesStartedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationStartedException;
+import ar.edu.itba.paw.services.exceptions.matchParticipation.MatchParticipationUnauthenticatedException;
 import ar.edu.itba.paw.webapp.utils.SecurityControllerUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,14 +46,30 @@ public class PlayerParticipationController {
             @PathVariable("matchId") final Long matchId,
             final RedirectAttributes redirectAttributes) {
         final User user = SecurityControllerUtils.requireAuthenticatedUser();
+        String errorCode = null;
 
         try {
             matchParticipationService.requestToJoin(matchId, user);
             redirectAttributes.addFlashAttribute("joinRequested", true);
             return new ModelAndView("redirect:/matches/" + matchId);
-        } catch (final MatchParticipationException e) {
-            return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + e.getCode());
+        } catch (final MatchParticipationUnauthenticatedException e) {
+            errorCode = "unauthenticated";
+        } catch (final MatchParticipationIsHostException e) {
+            errorCode = "is_host";
+        } catch (final MatchParticipationClosedException e) {
+            errorCode = "closed";
+        } catch (final MatchParticipationNotInviteOnlyException e) {
+            errorCode = "not_invite_only";
+        } catch (final MatchParticipationStartedException e) {
+            errorCode = "started";
+        } catch (final MatchParticipationAlreadyJoinedException e) {
+            errorCode = "already_joined";
+        } catch (final MatchParticipationAlreadyPendingException e) {
+            errorCode = "already_pending";
+        } catch (final MatchParticipationFullException e) {
+            errorCode = "full";
         }
+        return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + errorCode);
     }
 
     @PostMapping({
@@ -45,14 +80,30 @@ public class PlayerParticipationController {
             @PathVariable("matchId") final Long matchId,
             final RedirectAttributes redirectAttributes) {
         final User user = SecurityControllerUtils.requireAuthenticatedUser();
+        String errorCode = null;
 
         try {
             matchParticipationService.requestToJoinSeries(matchId, user);
             redirectAttributes.addFlashAttribute("seriesJoinRequested", true);
             return new ModelAndView("redirect:/matches/" + matchId);
-        } catch (final MatchParticipationException e) {
-            return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + e.getCode());
+        } catch (final MatchParticipationUnauthenticatedException e) {
+            errorCode = "unauthenticated";
+        } catch (final MatchParticipationIsHostException e) {
+            errorCode = "is_host";
+        } catch (final MatchParticipationNotRecurringException e) {
+            errorCode = "not_recurring";
+        } catch (final MatchParticipationSeriesStartedException e) {
+            errorCode = "series_started";
+        } catch (final MatchParticipationSeriesClosedException e) {
+            errorCode = "series_closed";
+        } catch (final MatchParticipationSeriesAlreadyJoinedException e) {
+            errorCode = "series_already_joined";
+        } catch (final MatchParticipationSeriesAlreadyPendingException e) {
+            errorCode = "series_already_pending";
+        } catch (final MatchParticipationSeriesFullException e) {
+            errorCode = "series_full";
         }
+        return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + errorCode);
     }
 
     @PostMapping("/matches/{matchId}/join-requests/cancel")
@@ -60,14 +111,16 @@ public class PlayerParticipationController {
             @PathVariable("matchId") final Long matchId,
             final RedirectAttributes redirectAttributes) {
         final User user = SecurityControllerUtils.requireAuthenticatedUser();
+        String errorCode = null;
 
         try {
             matchParticipationService.cancelJoinRequest(matchId, user);
             redirectAttributes.addFlashAttribute("joinStatus", "cancelled");
             return new ModelAndView("redirect:/matches/" + matchId);
-        } catch (final MatchParticipationException e) {
-            return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + e.getCode());
+        } catch (final MatchParticipationNoPendingRequestException e) {
+            errorCode = "no_pending_request";
         }
+        return new ModelAndView("redirect:/matches/" + matchId + "?joinError=" + errorCode);
     }
 
     @PostMapping("/matches/{matchId}/invites/accept")
@@ -75,25 +128,43 @@ public class PlayerParticipationController {
             @PathVariable("matchId") final Long matchId,
             final RedirectAttributes redirectAttributes) {
         final User user = SecurityControllerUtils.requireAuthenticatedUser();
+        String errorCode = null;
 
         try {
             matchParticipationService.acceptInvite(matchId, user);
             redirectAttributes.addFlashAttribute("inviteStatus", "accepted");
             return new ModelAndView("redirect:/matches/" + matchId);
-        } catch (final MatchParticipationException e) {
-            return new ModelAndView("redirect:/matches/" + matchId + "?inviteError=" + e.getCode());
+        } catch (final MatchParticipationNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (final MatchParticipationInvalidUserException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        } catch (final MatchParticipationIsHostException e) {
+            errorCode = "is_host";
+        } catch (final MatchParticipationNoInvitationException e) {
+            errorCode = "no_invitation";
+        } catch (final MatchParticipationClosedException e) {
+            errorCode = "closed";
+        } catch (final MatchParticipationStartedException e) {
+            errorCode = "started";
         }
+        return new ModelAndView("redirect:/matches/" + matchId + "?inviteError=" + errorCode);
     }
 
     @PostMapping("/matches/{matchId}/invites/decline")
     public ModelAndView declineInvite(@PathVariable("matchId") final Long matchId) {
         final User user = SecurityControllerUtils.requireAuthenticatedUser();
+        String errorCode = null;
 
         try {
             matchParticipationService.declineInvite(matchId, user);
             return new ModelAndView("redirect:/events");
-        } catch (final MatchParticipationException e) {
-            return new ModelAndView("redirect:/matches/" + matchId + "?inviteError=" + e.getCode());
+        } catch (final MatchParticipationNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (final MatchParticipationInvalidUserException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        } catch (final MatchParticipationNoInvitationException e) {
+            errorCode = "no_invitation";
         }
+        return new ModelAndView("redirect:/matches/" + matchId + "?inviteError=" + errorCode);
     }
 }
