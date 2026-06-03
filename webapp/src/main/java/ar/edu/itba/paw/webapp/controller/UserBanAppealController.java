3a5d574b489a608e668d2d7a3e656f3ea6062365
acragno <acragno@itba.edu.ar>
2026-06-03 14:39:46 -0300

    refactor: handle new exceptions in UserBanAppealController

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserBanAppealController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserBanAppealController.java
@@ -4,7 +4,9 @@ import ar.edu.itba.paw.models.ModerationReport;
 import ar.edu.itba.paw.models.User;
 import ar.edu.itba.paw.models.UserBan;
 import ar.edu.itba.paw.services.ModerationService;
-import ar.edu.itba.paw.services.exceptions.ModerationException;
+import ar.edu.itba.paw.services.exceptions.moderation.ModerationAppealLimitException;
+import ar.edu.itba.paw.services.exceptions.moderation.ModerationAppealRejectedException;
+import ar.edu.itba.paw.services.exceptions.moderation.ModerationReportNotFoundException;
 import ar.edu.itba.paw.webapp.utils.SecurityControllerUtils;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
@@ -84,12 +86,18 @@ public class UserBanAppealController {
                 moderationService
                         .findActiveBan(user)
                         .orElseThrow(() -> new ResponseStatusException(HttpStatus.FORBIDDEN));
+        String exceptionReason;
         try {
             moderationService.appealReport(activeBan.getModerationReport().getId(), appealReason);
             redirectAttributes.addFlashAttribute("action", "appealed");
             return new ModelAndView("redirect:/account/ban");
-        } catch (final ModerationException exception) {
-            return new ModelAndView("redirect:/account/ban?error=" + exception.getCode());
+        } catch (final ModerationReportNotFoundException exception) {
+            exceptionReason = "report_not_found";
+        } catch (final ModerationAppealLimitException exception) {
+            exceptionReason = "appeal_limit";
+        } catch (final ModerationAppealRejectedException exception) {
+            exceptionReason = "appeal_rejected";
         }
+        return new ModelAndView("redirect:/account/ban?error=" + exceptionReason);
     }
 }
