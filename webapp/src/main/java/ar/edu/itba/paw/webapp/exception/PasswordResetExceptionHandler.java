772de1c31fa10b5ab823a8b57ec1c11989085faf
acragno <acragno@itba.edu.ar>
2026-06-07 00:01:46 -0300

    refactor: simplify exception handling in PasswordResetController

--- webapp/src/main/java/ar/edu/itba/paw/webapp/exception/PasswordResetExceptionHandler.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exception/PasswordResetExceptionHandler.java
@@ -1,7 +1,8 @@
 package ar.edu.itba.paw.webapp.exception;
 
+import ar.edu.itba.paw.services.exceptions.registration.PasswordInvalidException;
 import ar.edu.itba.paw.services.exceptions.verificationFailure.VerificationFailureException;
-import ar.edu.itba.paw.webapp.controller.VerificationController;
+import ar.edu.itba.paw.webapp.controller.PasswordResetController;
 import ar.edu.itba.paw.webapp.utils.VerificationViews;
 import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +11,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice(assignableTypes = VerificationController.class)
-public class VerificationExceptionHandler {
+@ControllerAdvice(assignableTypes = PasswordResetController.class)
+public class PasswordResetExceptionHandler {
 
     private final MessageSource messageSource;
 
     @Autowired
-    public VerificationExceptionHandler(final MessageSource messageSource) {
+    public PasswordResetExceptionHandler(final MessageSource messageSource) {
         this.messageSource = messageSource;
     }
 
     @ExceptionHandler(VerificationFailureException.class)
     public ModelAndView handleVerificationFailure(
             final VerificationFailureException exception, final Locale locale) {
-        return VerificationViews.buildErrorView(exception.getMessage(), messageSource, locale, "/");
+        return VerificationViews.buildErrorView(
+                exception.getMessage(), messageSource, locale, "/forgot-password");
+    }
+
+    @ExceptionHandler(PasswordInvalidException.class)
+    public ModelAndView handlePasswordInvalid(
+            final PasswordInvalidException exception, final Locale locale) {
+        return VerificationViews.buildErrorView(
+                exception.getMessage(), messageSource, locale, "/forgot-password");
     }
 }
