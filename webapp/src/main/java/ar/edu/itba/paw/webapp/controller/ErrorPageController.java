e9b7c5d0159abc09d805c80aafdbe3df6e9b2575
acragno <acragno@itba.edu.ar>
2026-07-28 15:16:59 -0300

    refactor: replace @GetMapping with @RequestMapping in ErrorPageController and add POST tests for error routes

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorPageController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorPageController.java
@@ -3,44 +3,44 @@ package ar.edu.itba.paw.webapp.controller;
 import ar.edu.itba.paw.webapp.utils.ErrorPageViews;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ErrorPageController {
 
-    @GetMapping("/errors/404")
+    @RequestMapping("/errors/404")
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ModelAndView showNotFoundPage() {
         return ErrorPageViews.build("404");
     }
 
-    @GetMapping("/errors/400")
+    @RequestMapping("/errors/400")
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ModelAndView showBadRequestPage() {
         return ErrorPageViews.build("400");
     }
 
-    @GetMapping("/errors/405")
+    @RequestMapping("/errors/405")
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ModelAndView showMethodNotAllowedPage() {
         return ErrorPageViews.build("405");
     }
 
-    @GetMapping("/errors/403")
+    @RequestMapping("/errors/403")
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ModelAndView showForbiddenPage() {
         return ErrorPageViews.build("403");
     }
 
-    @GetMapping("/errors/409")
+    @RequestMapping("/errors/409")
     @ResponseStatus(HttpStatus.CONFLICT)
     public ModelAndView showConflictPage() {
         return ErrorPageViews.build("409");
     }
 
-    @GetMapping("/errors/500")
+    @RequestMapping("/errors/500")
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView showInternalServerErrorPage() {
         return ErrorPageViews.build("500");
--- webapp/src/test/java/ar/edu/itba/paw/webapp/controller/ErrorPageControllerTest.java
+++ webapp/src/test/java/ar/edu/itba/paw/webapp/controller/ErrorPageControllerTest.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -62,6 +63,54 @@ class ErrorPageControllerTest {
     @Test
     void getInternalServerErrorRouteRenders500Page() throws Exception {
         mockMvc.perform(get("/errors/500"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "500"));
+    }
+
+    @Test
+    void postNotFoundErrorRouteRenders404Page() throws Exception {
+        mockMvc.perform(post("/errors/404"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "404"));
+    }
+
+    @Test
+    void postBadRequestErrorRouteRenders400Page() throws Exception {
+        mockMvc.perform(post("/errors/400"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "400"));
+    }
+
+    @Test
+    void postMethodNotAllowedErrorRouteRenders405Page() throws Exception {
+        mockMvc.perform(post("/errors/405"))
+                .andExpect(status().isMethodNotAllowed())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "405"));
+    }
+
+    @Test
+    void postForbiddenErrorRouteRenders403Page() throws Exception {
+        mockMvc.perform(post("/errors/403"))
+                .andExpect(status().isForbidden())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "403"));
+    }
+
+    @Test
+    void postConflictErrorRouteRenders409Page() throws Exception {
+        mockMvc.perform(post("/errors/409"))
+                .andExpect(status().isConflict())
+                .andExpect(view().name("errors/error-page"))
+                .andExpect(model().attribute("number", "409"));
+    }
+
+    @Test
+    void postInternalServerErrorRouteRenders500Page() throws Exception {
+        mockMvc.perform(post("/errors/500"))
                 .andExpect(status().isInternalServerError())
                 .andExpect(view().name("errors/error-page"))
                 .andExpect(model().attribute("number", "500"));
