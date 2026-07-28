e2ec8aa460715a3632092da0bfe9e97cef1b06b7
acragno <acragno@itba.edu.ar>
2026-07-28 17:37:30 -0300

    fix: scan exception package so @ControllerAdvice handlers register and return proper HTTP statuses

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -71,6 +71,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ComponentScan({
     "ar.edu.itba.paw.webapp.controller",
+    "ar.edu.itba.paw.webapp.exception",
     "ar.edu.itba.paw.services",
     "ar.edu.itba.paw.persistence"
 })
